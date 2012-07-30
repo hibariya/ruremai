1fe91e7df8d508079568bb39f3e018f60bd91d30
Sho Hashimoto <sho-h@netlab.jp>
2012-07-30 20:17:38 +0900

    support class method

--- lib/ruremai/locator/rurema.rb
+++ lib/ruremai/locator/rurema.rb
@@ -12,15 +12,17 @@ module Ruremai
             uri_parts << CGI.escape(receiver.name).gsub(/(%[\dA-Z]{2})/) {|s|
               %(=#{s[1..-1].downcase})
             }
+            types = %w(s m i)
           else
             uri_parts << CGI.escape(receiver.class.name).gsub(/(%[\dA-Z]{2})/) {|s|
               %(=#{s[1..-1].downcase})
             }
+            types = %w(i m s)
           end
           uri_part = uri_parts.join('/')
           method_name = CGI.escape(name.to_s).gsub(/(%[\dA-Z]+)/) {|s| %(=#{s[1..-1].downcase}) }
 
-          %w(i s m).map {|type|
+          types.map {|type|
             URI.parse("#{uri_base}#{uri_part}/#{type}/#{method_name}.html")
           }
         end
