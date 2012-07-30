ed6de7c20e3b14246e2a6781b3dce57b6ee5b2bf
Sho Hashimoto <sho-h@netlab.jp>
2012-07-30 18:20:49 +0900

    support class method

--- lib/ruremai/locator/rurema.rb
+++ lib/ruremai/locator/rurema.rb
@@ -7,7 +7,13 @@ module Ruremai
         end
 
         def candidates
-          uri_part    = [RUBY_VERSION, 'method', owner.name.gsub(/::/, '/')].join('/')
+          uri_parts = [RUBY_VERSION, 'method']
+          if @method.receiver.instance_of?(Class)
+            uri_parts.push(receiver.name.gsub(/::/, '/'))
+          else
+            uri_parts.push(receiver.class.name.gsub(/::/, '/'))
+          end
+          uri_part = uri_parts.join('/')
           method_name = CGI.escape(name.to_s).gsub(/(%[\dA-Z]+)/) {|s| %(=#{s[1..-1].downcase}) }
 
           %w(i s m).map {|type|
