d5f9783f97137cb2c62523a9511a7c9161458538
hibariya <celluloid.key@gmail.com>
2013-09-01 17:09:57 +0900

    Add specs for operator methods

--- spec/locator/rurema_spec.rb
+++ spec/locator/rurema_spec.rb
@@ -6,28 +6,46 @@ describe Ruremai::Locator::Rurema do
 
     subject { locator.candidates.map(&:to_s) }
 
-    context 'CamelCase method' do
+    describe 'CamelCase method' do
       let(:target) { method(:Integer) }
 
       it { should include %(http://doc.ruby-lang.org/ja/#{RUBY_VERSION}/method/Kernel/m/Integer.html) }
     end
 
-    context 'query method' do
+    describe 'query method' do
       let(:target) { nil.method(:nil?) }
 
       it { should include %(http://doc.ruby-lang.org/ja/#{RUBY_VERSION}/method/NilClass/i/nil=3f.html) }
     end
 
-    context 'space ship operator' do
+    describe 'space ship operator' do
       let(:target) { 0.method(:<=>) }
 
       it { should include %(http://doc.ruby-lang.org/ja/#{RUBY_VERSION}/method/Fixnum/i/=3c=3d=3e.html) }
     end
 
-    context 'nested class' do
+    describe 'nested class' do
       let(:target) { Net::HTTP.method(:new) }
 
       it { should include %(http://doc.ruby-lang.org/ja/#{RUBY_VERSION}/method/Net=3a=3aHTTP/s/new.html) }
+    end
+
+    describe '`' do
+      let(:target) { Kernel.method(:`) }
+
+      it { should include %(http://doc.ruby-lang.org/ja/#{RUBY_VERSION}/method/Kernel/m/=60.html) }
+    end
+
+    describe 'infix operator method' do
+      let(:target) { 1.method(:+) }
+
+      it { should include %(http://doc.ruby-lang.org/ja/#{RUBY_VERSION}/method/Fixnum/i/=2b.html) }
+    end
+
+    describe 'prefix operator method' do
+      let(:target) { 1.method(:+@) }
+
+      it { should include %(http://doc.ruby-lang.org/ja/#{RUBY_VERSION}/method/Numeric/i/=2b=40.html) }
     end
   end
 end
