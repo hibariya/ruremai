4ca9e2dd8f1160ba80d83a736d83febc083ebb8c
hibariya <celluloid.key@gmail.com>
2013-09-07 21:58:59 +0900

    Extract module

--- lib/ruremai/ext/method.rb
+++ lib/ruremai/ext/method.rb
@@ -1,9 +1,15 @@
-class Method
-  def rurema!(options = {})
-    if locale = options[:locale]
-      Ruremai.launch self, Array(locale)
-    else
-      Ruremai.launch self
+module Ruremai
+  module MethodMixin
+    def rurema!(options = {})
+      if locale = options[:locale]
+        Ruremai.launch self, Array(locale)
+      else
+        Ruremai.launch self
+      end
     end
   end
+end
+
+class Method
+  include Ruremai::MethodMixin
 end
--- lib/ruremai/ext/object.rb
+++ lib/ruremai/ext/object.rb
@@ -1,13 +1,19 @@
 require 'ruremai/mean'
 
-class Object
-  def mean?(*args)
-    if args.empty?
-      return Ruremai::Mean.new(self)
-    else
-      args.each do |method|
-        self.method(method.to_sym).rurema!
+module Ruremai
+  module ObjectMixin
+    def mean?(*args)
+      if args.empty?
+        return Ruremai::Mean.new(self)
+      else
+        args.each do |method|
+          self.method(method.to_sym).rurema!
+        end
       end
     end
   end
+end
+
+class Object
+  include Ruremai::ObjectMixin
 end
